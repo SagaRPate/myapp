74dcdb6b7f8f76a2d5ef87628a2a4f8c3c8c6137
Sawan <sawan@kartuminfotech.com>
2017-12-04 11:36:49 +0530

    First time setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {AppRegistry, Text} from 'react-native';
+
+const myapp = () => App(
+    <Text style={styles.welcome}>
+      Welcome To My App
+      Micra Solution
+    </Text>
+)
+
+AppRegistry.registerComponent('myapp', () => app);
