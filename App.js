ac4162aab69b235c28836a0089e211083da36520
$@G@R <34228088+kachhadiyasagar000@users.noreply.github.com>
2017-12-08 18:28:02 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -10,39 +10,68 @@ import {
   Text,
   View,
   Image,
-  TextInput
+  TextInput,
+  TouchableOpacity,
+  Switch,
+  Linking,
  } from 'react-native';
 
+
 export default class App extends Component<{}> {
-       constructor(props) {
-           super(props);
-           this.state = {text: ''};
-       }
+
+  constructor(props) {
+    super(props);
+    this.toggleSwitch = this.toggleSwitch.bind(this);
+    this.state = { showPassword: true, };
+  }
+
+
+  toggleSwitch() {
+    this.setState({ showPassword: !this.state.showPassword });
+  }
+
       render() {
         return (
+
           <View style={styles.container}>
             <Image style={styles.image} source={require('./img/logo.png')} />
+              <View style={{ flexDirection: 'row', borderWidth: 1, marginLeft: 30, width: 295, borderBottomColor: '#fff', }}>
+                <TextInput  style={styles.TextInputEmail}
+                  placeholder="Email id..."
+                  placeholderTextColor="#fff"
+                  underlineColorAndroid="#000"
+                  onChangeText={(text) => this.setState({text})}
+                />
+              </View>
+                <View style={{ flexDirection: 'row', borderWidth: 1, marginLeft: 30, width: 295, borderBottomColor: '#fff', }}>
+                    <TextInput style={styles.Password}
+                        placeholder="Password..."
+                        placeholderTextColor="#fff"
+                        underlineColorAndroid="#000"
+                        style={styles.TextInputPassword}
+                        secureTextEntry={true}
+                        secureTextEntry={this.state.showPassword}
+                        onChangeText={(password) => this.setState({ password })}
+                      />
+                      <Switch style={styles.PasswordSwitch}
+                        onValueChange={this.toggleSwitch}
+                        value={!this.state.showPassword}
+                      />
+                </View>
+                <TouchableOpacity>
+                    <Text style={styles.button}
+                        onPress={() => Linking.openURL('https://www.google.co.in')}>
+                        Login
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                     <Text style={styles.buttonpassword}>
+                        Forget Password
+                     </Text>
+                </TouchableOpacity>
 
-            <TextInput style={styles.TextInput}
-              placeholder="Email id"
-              placeholderTextColor="white"
-              underlineColorAndroid="#fff"
-              onChangeText={(text) => this.setState({text})}
-            />
-
-            <TextInput style={styles.TextInput}
-              placeholder="Password"
-              placeholderTextColor="#fff"
-              underlineColorAndroid="#fff"
-              onChangeText={(text) => this.setState({text})}
-            />
-              <Text style={styles.Text_Button}>
-                login
-              </Text>
-              <Text style={styles.Text_Button}>
-                forget Password
-              </Text>
           </View>
+
         );
       }
     }
@@ -63,36 +92,57 @@ const styles = StyleSheet.create({
         fontSize: 25,
         color: '#3bc4f2',
       },
-
-      TextInput : {
+      TextInputEmail: {
+        width: 215,
         marginTop: 30,
-        marginLeft: 30,
         marginRight: 30,
-        fontSize: 16,
+        fontSize: 18,
         color: '#fff',
       },
-      Button: {
-        width:200,
-        marginLeft:100,
-        fontSize: 25,
-        color: 'green',
-        padding:10, 
-        height:60,
-        borderRadius:4,
-        backgroundColor: 'white',
-        marginTop:30,
-        textAlign:'center',   
+      TextInputPassword: {
+        width: 215,
+        marginTop: 30,
+        marginRight: 30,
+        fontSize: 18,
+        color: '#fff',
       },
-      Text_Button: {
-        width:300,
-        marginLeft:50,
+     Password :{
+         justifyContent: 'center',
+         flex:1,
+         color: '#fff',
+     },
+      button: {
+        color: '#1d3d89',
         fontSize: 25,
-        color: 'green',
-        padding:10, 
-        height:60,
-        borderRadius:4,
-        backgroundColor: 'white',
-        marginTop:30,
-        textAlign:'center', 
+        marginLeft: 100,
+        marginTop: 30,
+        borderRadius: 8,
+        paddingTop: 8,
+        paddingLeft: 45,
+        width: 150,
+        height: 50,
+        backgroundColor: '#3bc3f2',
+        marginBottom: 10,
+      },
+      buttonpassword: {
+        color: 'red',
+        backgroundColor: '#3bc3f2',
+        fontSize: 25,
+        width: 235,
+        marginLeft: 60,
+        marginTop: 10,
+        paddingLeft: 25,
+        paddingTop: 8,
+        paddingBottom: 8,
+        borderRadius: 8,
+      },
+      PasswordSwitch: {
+        height: 40,
+        width: 50,
+        marginTop: 40,
       },
   });
+
+
+
+
